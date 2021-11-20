c71f2e14ff8a898f015cc583ddc728f231c3dbdb
Dana Chen <danaqchen@gmail.com>
2021-11-20 22:22:21 +0100

    validation

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name = 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
   
   next(error)
@@ -98,9 +100,12 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+        .then(savedPerson => savedPerson.toJSON())
+        .then(savedAndFormmattedPerson => {
+          response.json(savedAndFormmattedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.use(errorHandler)
