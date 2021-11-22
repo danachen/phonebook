cb8a25c9ef3abb4c584baded57d8f5f583b6b29a
Dana Chen <danaqchen@gmail.com>
2021-11-22 12:32:29 +0100

    update file structure

--- models/person.js
+++ models/person.js
@@ -1,17 +1,4 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,8 +12,6 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
   }
 })
-
-personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
